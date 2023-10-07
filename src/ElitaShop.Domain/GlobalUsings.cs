45fa607292d4ff13da72fab78a99bdc627ba5cc2
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-10-07 15:48:30 +0500

    domain uchun global using ishlatdim

---
+++ src/ElitaShop.Domain/GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿global using System.ComponentModel.DataAnnotations.Schema;
+global using ElitaShop.Domain.Entities.Products;
+global using ElitaShop.Domain.Entities.Carts;
+global using ElitaShop.Domain.Entities.Orders;
+global using ElitaShop.Domain.Entities.Users;
+global using ElitaShop.Domain.Entities.Categories;
