4bddf4d2a1ec21d2d9824637665814bc43e35a2b
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-10-07 17:30:13 +0500

    Pagination model and GetSkipCount method📃

---
+++ src/ElitaShop.DataAccess/Helpers/Paginations/PaginationData.cs
@@ -0,0 +1,17 @@
+﻿namespace ElitaShop.DataAccess.Paginations
+{
+    public class PaginationData
+    {
+        public bool HasPrevious { get; set; }
+
+        public bool HasNext { get; set; }
+
+        public int CurrentPage { get; set; }
+
+        public int TotalPages { get; set; }
+
+        public int PageSize { get; set; }
+
+        public int TotalItems { get; set; }
+    }
+}
---
+++ src/ElitaShop.DataAccess/Helpers/Paginations/PaginationParams.cs
@@ -0,0 +1,20 @@
+﻿namespace ElitaShop.DataAccess.Paginations
+{
+    public class PaginationParams
+    {
+        public int PageSize { get; set; }
+        public int PageNumber { get; set; }
+        public int PageSiza { get; }
+
+        public PaginationParams(int pageSiza, int pageNumber)
+        {
+            PageSiza = pageSiza;
+            PageNumber = pageNumber;
+        }
+
+        public int GetSkipCount()
+        {
+            return (PageNumber - 1) * PageSize;
+        }
+    }
+}
