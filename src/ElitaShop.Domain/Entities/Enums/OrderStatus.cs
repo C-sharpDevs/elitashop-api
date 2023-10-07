834a9348608951e4fd07f99e7505deeaa9ad342a
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-10-07 15:47:48 +0500

    card => cart yozilmagan status ni qo'shdim

--- src/ElitaShop.Domain/Entities/Enums/OrderStatus.cs
+++ src/ElitaShop.Domain/Entities/Enums/OrderStatus.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ElitaShop.Domain.Entities.Enums
+﻿namespace ElitaShop.Domain.Entities.Enums
 {
     public enum OrderStatus
     {
+        Order,
         Checkout,
         Paid,
         Failed,
