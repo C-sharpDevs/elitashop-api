9fccd9db114ab283494af439ed7daba40344337f
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-10-10 09:53:44 +0500

    #36 close

---
+++ src/ElitaShop.Services/Dtos/Orders/OrderItemCreateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ElitaShop.Services.Dtos.Orders
+{
+    public class OrderItemCreateDto
+    {
+        public decimal Price { get; set; }
+        public decimal Discount { get; set; }
+        public int Quantity { get; set; }
+    }
+}
