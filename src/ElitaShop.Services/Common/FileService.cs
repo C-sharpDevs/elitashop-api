6f8eb0f363dc74c2e5d9fdd9b059787e9586db4e
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-10-10 16:12:37 +0500

    location changing

---
+++ src/ElitaShop.Services/Common/FileService.cs
@@ -0,0 +1,67 @@
+﻿using ElitaShop.Services.Interfaces.Common;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Hosting;
+using ElitaShop.Services.Common.Helpers;
+
+namespace ElitaShop.Services.Common
+{
+    public class FileService : IFileService
+    {
+
+        private readonly string MEDIA = "media";
+        private readonly string AVATARS = "avatars";
+        private readonly string IMAGES = "images";
+        private readonly string ROOTPATH;
+        public FileService()
+        {
+            ROOTPATH = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            Directory.GetDirectoryRoot("");
+        }
+
+        public async Task<bool> DeleteAvatarAsync(string file)
+        {
+            return await DeleteAvatarAsync(file);
+        }
+
+        public async Task<bool> DeleteImageAsync(string file)
+        {
+            string path = Path.Combine(ROOTPATH, file);
+            if (File.Exists(path))
+            {
+                await Task.Run(() =>
+                {
+                    File.Delete(path);
+                });
+                return true;
+            }
+            return false;
+            
+        }
+
+        public async Task<string> UploadAvatarAsync(IFormFile file)
+        {
+            string newAvatarName = MediaHelper.MakeImageName(file.FileName);
+            string subPath = Path.Combine(MEDIA, AVATARS, newAvatarName);
+            string path = Path.Combine(ROOTPATH, subPath);
+
+            using(var stream = new FileStream(path, FileMode.Open))
+            {
+                await file.CopyToAsync(stream);
+                return subPath;
+            }
+        }
+
+        public async Task<string> UploadImageAsync(IFormFile file)
+        {
+            string newImageName = MediaHelper.MakeImageName(file.FileName);
+            string subPath = Path.Combine(MEDIA, IMAGES, newImageName);
+            string path = Path.Combine(ROOTPATH, subPath);
+
+            using (var stream = new FileStream(path, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+                return subPath;
+            }
+        }
+    }
+}
