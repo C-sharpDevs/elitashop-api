1270338a40ad6aaa0810ad808df26aa5b226a0fd
Berdikulov-571 <bsanjarbek06@gmail.com>
2023-10-10 11:01:15 +0500

    MediaHelperCreated

---
+++ src/ElitaShop.Services/Common/Helpers/MediaHelper.cs
@@ -0,0 +1,35 @@
+﻿namespace ElitaShop.Services.Common.Helpers
+{
+    public class MediaHelper
+    {
+        public static string MakeImageName(string filename)
+        {
+            FileInfo fileInfo = new FileInfo(filename);
+
+            string[] ImageExtension = GetImageExtensions();
+
+            if(ImageExtension.Any(x => x == fileInfo.Extension))
+            {
+                string extension = fileInfo.Extension;
+                string name = "IMG_" + Guid.NewGuid() + extension;
+                return name;
+            }
+            return null;
+        }
+
+        public static string[] GetImageExtensions()
+        {
+            return new string[]
+            {
+            // JPG files
+            ".jpg", ".jpeg",
+            // Png files
+            ".png",
+            // Bmp files
+            ".bmp",
+            // Svg files
+            ".svg"
+            };
+        }
+    }
+}
