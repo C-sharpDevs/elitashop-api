ae8d5d7dc5667436e06bb2e8a8b853ebff6b3f63
Berdikulov-571 <bsanjarbek06@gmail.com>
2023-10-10 10:32:50 +0500

    UserDto Created

---
+++ src/ElitaShop.Services/Dtos/User/UserCreatedDto.cs
@@ -0,0 +1,17 @@
+﻿using ElitaShop.Domain.Entities.Carts;
+using ElitaShop.Domain.Entities.Orders;
+
+namespace ElitaShop.Services.Dtos.User
+{
+    public class UserCreatedDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string? Intro { get; set; }
+        public string? UserImage { get; set; }
+    }
+}
---
+++ src/ElitaShop.Services/Dtos/User/UserUpdateDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElitaShop.Services.Dtos.User
+{
+    public class UserUpdateDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string? Intro { get; set; }
+        public string? UserImage { get; set; }
+    }
+}
