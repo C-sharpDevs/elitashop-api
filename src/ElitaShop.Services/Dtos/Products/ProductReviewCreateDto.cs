9d69937c982c8a07c623b9bc72b3563a45f8e4bd
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-10-10 11:22:14 +0500

    ProductCreateDto ProductUpdateDto ProductReviewCreateDto ProductReviewUpdateDto

--- src/ElitaShop.Services/Dtos/Products/ProductReviewCreateDto.cs
+++ src/ElitaShop.Services/Dtos/Products/ProductReviewCreateDto.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ElitaShop.Services.Dtos.Products
+﻿namespace ElitaShop.Services.Dtos.Products
 {
     public class ProductReviewCreateDto
     {
