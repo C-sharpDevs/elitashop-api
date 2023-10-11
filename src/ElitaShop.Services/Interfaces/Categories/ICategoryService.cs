56e4df73759b608d4864e4b22f0e9c92c803af9e
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-10-11 15:49:41 +0500

    Rename dto class CategoryCreateDto

---
+++ src/ElitaShop.Services/Interfaces/Categories/ICategoryService.cs
@@ -0,0 +1,17 @@
+﻿using ElitaShop.DataAccess.Paginations;
+
+namespace ElitaShop.Services.Interfaces.Categories
+{
+    public interface ICategoryService
+    {
+        Task<bool> CreateAsync(CategoryCreateDto categoryCreateDto);
+        
+        Task<bool> UpdateAsync(CategoryUpdateDto categoryUpdateDto);
+        
+        Task<bool> DeleteAsync(long categoryId);
+        
+        Task<Category> GetByIdAsync(long categoryId);
+        
+        Task<Category> GetAllAsync(PaginationParams @params);
+    }
+}
