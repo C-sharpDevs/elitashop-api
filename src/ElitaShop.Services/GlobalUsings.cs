20b2baa6c556a12b5870c217f61f0453e7cac476
Berdikulov-571 <bsanjarbek06@gmail.com>
2023-10-11 21:29:39 +0500

    ControllerCreated

--- src/ElitaShop.Services/GlobalUsings.cs
+++ src/ElitaShop.Services/GlobalUsings.cs
@@ -1,15 +1,19 @@
-﻿global using AutoMapper;
-global using ElitaShop.DataAccess.Paginations;
+﻿global using ElitaShop.Domain.Enums;
+global using Microsoft.AspNetCore.Http;
+global using AutoMapper;
 global using ElitaShop.Domain.Entities.Carts;
 global using ElitaShop.Domain.Entities.Categories;
 global using ElitaShop.Domain.Entities.Orders;
 global using ElitaShop.Domain.Entities.Products;
 global using ElitaShop.Domain.Entities.Users;
-global using ElitaShop.Domain.Enums;
 global using ElitaShop.Services.Dtos.Carts;
 global using ElitaShop.Services.Dtos.Categories;
 global using ElitaShop.Services.Dtos.Orders;
 global using ElitaShop.Services.Dtos.Products;
 global using ElitaShop.Services.Dtos.User;
-global using Microsoft.AspNetCore.Http;
-
+global using ElitaShop.DataAccess.Interfaces.BaseRepositories;
+global using ElitaShop.DataAccess.Interfaces.EntityRepositories;
+global using ElitaShop.DataAccess.Paginations;
+global using ElitaShop.Domain.Exceptions.Products;
+global using ElitaShop.Domain.Exceptions.Images;
+global using ElitaShop.Services.Interfaces.Common;
