64c9456396d3f3bd078aba4414cb4a2d1c902932
Berdikulov-571 <bsanjarbek06@gmail.com>
2023-10-11 14:11:38 +0500

    IProductCategoryRepositoryCreated

---
+++ src/ElitaShop.Services/Interfaces/Product/IProductCategoryRepository.cs
@@ -0,0 +1,19 @@
+﻿using ElitaShop.DataAccess.Paginations;
+
+public interface IProductCategoryRepository
+{
+    // Append
+    Task<bool> AddProductToCategory(long productId,long categoryId);
+    Task<bool> AddRangeProductsToCategory(List<long> productIds, long categoryId);
+    
+    // Delete
+    Task<bool> Delete(long productId,long categoryId);
+    Task<bool> DeleteRange(List<long> productIds, long categoryId);
+
+    // Get
+    Task<IList<Product>> GetAllProductsInTheCategory(long categoryId,PaginationParams @params);
+
+    //Update
+    Task<bool> UpdateProductCategory(long productId, long oldCategoryId, long newCategoryId);
+    Task<bool> UpdateRangeProductsCategory(List<long> productIds, long oldCategoryId, long newCategoryId);
+}
