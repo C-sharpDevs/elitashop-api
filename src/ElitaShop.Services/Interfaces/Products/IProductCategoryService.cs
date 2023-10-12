facc95e03dcee941ee1f6538509c82d2a61a945a
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-10-12 21:16:55 +0500

    namespace fixed

--- src/ElitaShop.Services/Interfaces/Products/IProductCategoryService.cs
+++ src/ElitaShop.Services/Interfaces/Products/IProductCategoryService.cs
@@ -1,17 +1,20 @@
-﻿public interface IProductCategoryService
+﻿namespace ElitaShop.Services.Interfaces.Products
 {
-    // Append
-    Task<bool> AddProductToCategoryAsync(long productId, long categoryId);
-    Task<bool> AddRangeProductsToCategoryAsync(List<long> productIds, long categoryId);
+    public interface IProductCategoryService
+    {
+        // Append
+        Task<bool> AddProductToCategoryAsync(long productId, long categoryId);
+        Task<bool> AddRangeProductsToCategoryAsync(List<long> productIds, long categoryId);
 
-    // Delete
-    Task<bool> DeleteAsync(long productId, long categoryId);
-    Task<bool> DeleteRangeAsync(List<long> productIds, long categoryId);
+        // Delete
+        Task<bool> DeleteAsync(long productId, long categoryId);
+        Task<bool> DeleteRangeAsync(List<long> productIds, long categoryId);
 
-    // Get
-    Task<IEnumerable<Product>> GetAllProductsInTheCategoryAsync(long categoryId, PaginationParams @params);
+        // Get
+        Task<IEnumerable<Product>> GetAllProductsInTheCategoryAsync(long categoryId, PaginationParams @params);
 
-    //Update
-    Task<bool> UpdateProductCategoryAsync(long productId, long oldCategoryId, long newCategoryId);
-    Task<bool> UpdateRangeProductsCategoryAsync(List<long> productIds, long oldCategoryId, long newCategoryId);
+        //Update
+        Task<bool> UpdateProductCategoryAsync(long productId, long oldCategoryId, long newCategoryId);
+        Task<bool> UpdateRangeProductsCategoryAsync(List<long> productIds, long oldCategoryId, long newCategoryId);
+    }
 }
--- src/ElitaShop.Services/Interfaces/Products/IProductService.cs
+++ src/ElitaShop.Services/Interfaces/Products/IProductService.cs
@@ -1,15 +1,18 @@
-﻿public interface IProductService
+﻿namespace ElitaShop.Services.Interfaces.Products
 {
-    // Append
-    Task<bool> CreateAsync(ProductCreateDto productCreateDto);
+    public interface IProductService
+    {
+        // Append
+        Task<bool> CreateAsync(ProductCreateDto productCreateDto);
 
-    // Update
-    Task<bool> UpdateAsync(long productId, ProductUpdateDto productUpdateDto);
+        // Update
+        Task<bool> UpdateAsync(long productId, ProductUpdateDto productUpdateDto);
 
-    // Delete
-    Task<bool> DeleteAsync(long productId);
+        // Delete
+        Task<bool> DeleteAsync(long productId);
 
-    // Get
-    Task<IEnumerable<Product>> GetAllAsync(PaginationParams @params);
-    Task<Product> GetByIdAsync(long productId);
+        // Get
+        Task<IEnumerable<Product>> GetAllAsync(PaginationParams @params);
+        Task<Product> GetByIdAsync(long productId);
+    }
 }
