2aec536ae063d771b9353bc2a853f2e752dd1fc8
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-10-13 21:04:04 +0500

    mapping tayyor

--- src/ElitaShop.Services/Services/Common/AutoMapper/ApplicationProfile.cs
+++ src/ElitaShop.Services/Services/Common/AutoMapper/ApplicationProfile.cs
@@ -7,16 +7,50 @@
             #region Dto
             // Cart
             CreateMap<CartCreateDto, Cart>();
-            CreateMap<CartUpdateDto, Cart>();
+            CreateMap<CartUpdateDto, Cart>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .ForMember(dest => dest.CreatedAt, opt => opt.Ignore())
+                .ForMember(dest => dest.UserId, opt => opt.Ignore());
 
             // CartItem
-            CreateMap<CartItemCreateDto, Cart>();
-            CreateMap<CartItemUpdateDto, Cart>();
+            CreateMap<CartItemCreateDto, CartItem>();
+            CreateMap<CartItemUpdateDto, CartItem>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .ForMember(dest => dest.CreatedAt, opt => opt.Ignore())
+                .ForMember(dest => dest.ProductId, opt => opt.Ignore())
+                .ForMember(dest => dest.CartId, opt => opt.Ignore());
 
             // Category
             CreateMap<CategoryCreateDto, Category>();
-            CreateMap<CategoryUpdateDto, Category>();
+            CreateMap<CategoryUpdateDto, Category>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .ForMember(dest => dest.CreatedAt, opt => opt.Ignore());
 
+            // Product
+            CreateMap<ProductCreateDto, Product>();
+            CreateMap<ProductUpdateDto, Product>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .ForMember(dest => dest.CreatedAt, opt => opt.Ignore())
+                .ForMember(dest => dest.PublishedAt, opt => opt.Ignore())
+                .ForMember(dest => dest.StartAt, opt => opt.Ignore())
+                .ForMember(dest => dest.EndAt, opt => opt.Ignore());
+
+            // Product Review
+            CreateMap<ProductReviewCreateDto, ProductReview>();
+            CreateMap<ProductReviewUpdateDto, ProductReview>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .ForMember(dest => dest.CreatedAt, opt => opt.Ignore())
+                .ForMember(dest => dest.ProductId, opt => opt.Ignore());
+
+            // User
+            CreateMap<UserCreateDto, User>();
+            CreateMap<UserUpdateDto, User>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .ForMember(dest => dest.CreatedAt, opt => opt.Ignore())
+                .ForMember(dest => dest.Admin, opt => opt.Ignore())
+                .ForMember(dest => dest.Vendor, opt => opt.Ignore());
+            #endregion
+            #region order
             // Order
             CreateMap<OrderCreateDto, Order>();
             CreateMap<OrderUpdateDto, Order>();
@@ -24,49 +58,7 @@
             // OrderItem
             CreateMap<OrderItemCreateDto, OrderItem>();
             CreateMap<OrderItemUpdateDto, OrderItem>();
-
-            // Product
-            CreateMap<ProductCreateDto, Product>();
-            CreateMap<ProductUpdateDto, Product>();
-
-            // Product Review
-            CreateMap<ProductReviewCreateDto, ProductReview>();
-            CreateMap<ProductReviewUpdateDto, ProductReview>();
-
-            // User
-            CreateMap<UserCreateDto, User>();
-            CreateMap<UserUpdateDto, User>();
             #endregion
-            #region updates
-            CreateMap<Cart, Cart>()
-                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
-                .ForMember(dest => dest.CreatedAt, opt => opt.MapFrom(src => src.CreatedAt))
-                .ForMember(dest => dest.UserId, opt => opt.MapFrom(src => src.UserId));
-            CreateMap<CartItem, CartItem>()
-                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
-                .ForMember(dest => dest.CreatedAt, opt => opt.MapFrom(src => src.CreatedAt))
-                .ForMember(dest => dest.ProductId, opt => opt.MapFrom(src => src.ProductId))
-                .ForMember(dest => dest.CartId, opt => opt.MapFrom(src => src.CartId));
-            CreateMap<User, User>()
-                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
-                .ForMember(dest => dest.CreatedAt, opt => opt.MapFrom(src => src.CreatedAt))
-                .ForMember(dest => dest.Admin, opt => opt.MapFrom(src => src.Admin))
-                .ForMember(dest => dest.Vendor, opt => opt.MapFrom(src => src.Vendor));
-            CreateMap<Product, Product>()
-                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
-                .ForMember(dest => dest.CreatedAt, opt => opt.MapFrom(src => src.CreatedAt))
-                .ForMember(dest => dest.PublishedAt, opt => opt.MapFrom(src => src.PublishedAt))
-                .ForMember(dest => dest.StartAt, opt => opt.MapFrom(src => src.StartAt))
-                .ForMember(dest => dest.EndAt, opt => opt.MapFrom(src => src.EndAt));
-            CreateMap<Category, Category>()
-                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
-                .ForMember(dest => dest.CreatedAt, opt => opt.MapFrom(src => src.CreatedAt));
-
-
-
-            #endregion
-
-
 
         }
     }
