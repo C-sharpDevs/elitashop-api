7a0ebdc8d5d977556bdefa71b2b07430b6a23dd4
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-10-18 16:15:48 +0500

    Add migration update Product

---
+++ src/ElitaShop.DataAccess/Migrations/20231018111341_UpdateProductModel.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ElitaShop.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateProductModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Users_UserId",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_UserId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Products");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "UserId",
+                table: "Products",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_UserId",
+                table: "Products",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Users_UserId",
+                table: "Products",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
