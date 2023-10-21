85657fbd114ea5685732eb10e44a234924500519
Berdikulov-571 <mygithub571@gmail.com>
2023-10-21 13:26:42 +0500

    CustomPasswordAttribute Created

---
+++ src/ElitaShop.Services/CustomAttributes/PasswordAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace ElitaShop.Services.CustomAttributes
+{
+    [AttributeUsage(AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
+    public class PasswordAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            if (value == null)
+            {
+                return false;
+            }
+
+            string password = value.ToString();
+
+            if (Regex.IsMatch(password, @"^(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z\d\s]).{7,}$"))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
