8ebfa3e1438e9c44cebed85fca710d15dc49d35d
Berdikulov-571 <mygithub571@gmail.com>
2023-10-21 13:33:20 +0500

    ForOrderDtoAddedCustomeAttributes

--- src/ElitaShop.Services/Dtos/Orders/OrderUpdateDto.cs
+++ src/ElitaShop.Services/Dtos/Orders/OrderUpdateDto.cs
@@ -1,4 +1,5 @@
 ﻿using ElitaShop.Domain.Enums;
+using ElitaShop.Services.CustomAttributes;
 
 namespace ElitaShop.Services.Dtos.Orders
 {
@@ -7,10 +8,15 @@ namespace ElitaShop.Services.Dtos.Orders
         public OrderStatus Status { get; set; }
         public decimal SubTotal { get; set; }
         public decimal ItemDiscount { get; set; }
+        [Length]
         public string FirstName { get; set; } = string.Empty;
+        [Length]
         public string LastName { get; set; } = string.Empty;
+        [Length]
         public string MiddleName { get; set; } = string.Empty;
+        [PhoneNumber]
         public string PhoneNumber { get; set; } = string.Empty;
+        [Email]
         public string Email { get; set; } = string.Empty;
         public string Address1 { get; set; } = string.Empty;
         public string Address2 { get; set; } = string.Empty;
