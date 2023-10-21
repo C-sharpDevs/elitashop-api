7c424af85bb0f4cd666294cde5464460c39ecada
Berdikulov-571 <mygithub571@gmail.com>
2023-10-21 16:56:34 +0500

    Hash512 Added For UserService and UserDto

--- src/ElitaShop.Services/Dtos/Orders/OrderCreateDto.cs
+++ src/ElitaShop.Services/Dtos/Orders/OrderCreateDto.cs
@@ -8,15 +8,10 @@ namespace ElitaShop.Services.Dtos.Orders
         public OrderStatus Status { get; set; }
         public decimal SubTotal { get; set; }
         public decimal ItemDiscount { get; set; }
-        [Length]
         public string FirstName { get; set; } = string.Empty;
-        [Length]
         public string LastName { get; set; } = string.Empty;
-        [Length]
         public string MiddleName { get; set; } = string.Empty;
-        [PhoneNumber]
         public string PhoneNumber { get; set; } = string.Empty;
-        [Email]
         public string Email { get; set; } = string.Empty;
         public string Address1 { get; set; } = string.Empty;
         public string Address2 { get; set; } = string.Empty;
